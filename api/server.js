7836489dfa9a83b6f4697be77efc0436b839d45a
George Raymond <georgeraymond98@gmail.com>
2020-02-02 20:09:06 -0500

    update on sub branch

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const authRouter = require('../auth/auth-router.js');
+// const authRouter = require('../auth/auth-router.js');
 // const usersRouter = require('../users/users-router.js');
 
 const server = express();
